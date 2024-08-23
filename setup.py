e89f14a9c2c74536e9cfa69e6d63ca2cca0b422d
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2024-08-23 11:34:51 +0200

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Depth4MC',
+    version='1.0.0',
+    author='Julian BvW',
+    url='https://github.com/JulianBvW/Depth4MC',
+    description='Depth estimator machine learning model for the sandbox game Minecraft',
+    packages=find_packages()
+)
